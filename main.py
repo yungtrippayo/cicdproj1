360923b448e587c3715672c623cc1fc8a9389060
Никита Будовой <nikita.budovoy@knu.ua>
2023-05-30 16:08:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ class TicTacToe:
     def __init__(self):
         self.board = [' '] * 9
         self.current_player = 'X'
+
     def print_board(self):
         print(f'{self.board[0]} | {self.board[1]} | {self.board[2]}')
         print('---------')
         print(f'{self.board[3]} | {self.board[4]} | {self.board[5]}')
         print('---------')
         print(f'{self.board[6]} | {self.board[7]} | {self.board[8]}')
+
     def check_win(self):
         win_conditions = [(0, 1, 2), (3, 4, 5), (6, 7, 8),
                           (0, 3, 6), (1, 4, 7), (2, 5, 8),
@@ -20,13 +22,14 @@ class TicTacToe:
             return 'Draw'
         return False
 
- def get_move(self):
-            while True:
-                move = input(f'Player {self.current_player}, enter your move (1-9): ')
-                if not move.isdigit() or int(move) < 1 or int(move) > 9 or self.board[int(move) - 1] != ' ':
-                    print('Invalid move. Try again.')
-                else:
-                    return int(move) - 1
+    def get_move(self):
+        while True:
+            move = input(f'Player {self.current_player}, enter your move (1-9): ')
+            if not move.isdigit() or int(move) < 1 or int(move) > 9 or self.board[int(move) - 1] != ' ':
+                print('Invalid move. Try again.')
+            else:
+                return int(move) - 1
+
     def play(self):
         while True:
             self.print_board()
@@ -41,3 +44,8 @@ class TicTacToe:
                     print(f'Player {self.current_player} wins!')
                 break
             self.current_player = 'O' if self.current_player == 'X' else 'X'
+
+
+if __name__ == '__main__':
+    game = TicTacToe()
+    game.play()
