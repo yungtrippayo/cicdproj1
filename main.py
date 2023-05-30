32fb49e6e9682d859639302a13304a119ea89b76
Никита Будовой <nikita.budovoy@knu.ua>
2023-05-30 16:03:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ class TicTacToe:
         print(f'{self.board[3]} | {self.board[4]} | {self.board[5]}')
         print('---------')
         print(f'{self.board[6]} | {self.board[7]} | {self.board[8]}')
+    def check_win(self):
+        win_conditions = [(0, 1, 2), (3, 4, 5), (6, 7, 8),
+                          (0, 3, 6), (1, 4, 7), (2, 5, 8),
+                          (0, 4, 8), (2, 4, 6)]
+
+        for condition in win_conditions:
+            if self.board[condition[0]] == self.board[condition[1]] == self.board[condition[2]] != ' ':
+                return self.board[condition[0]]
+        if ' ' not in self.board:
+            return 'Draw'
+        return False
