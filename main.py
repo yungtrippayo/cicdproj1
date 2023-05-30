5b1ef009d35b7e05b4804e42c3a6a468fbc7c721
Nikita Hodun <129158012+yungtrippayo@users.noreply.github.com>
2023-05-30 16:04:26 +0300

    додано функція ходів

--- main.py
+++ main.py
@@ -9,3 +9,23 @@ class TicTacToe:
         print(f'{self.board[3]} | {self.board[4]} | {self.board[5]}')
         print('---------')
         print(f'{self.board[6]} | {self.board[7]} | {self.board[8]}')
+
+        def check_win(self):
+            win_conditions = [(0, 1, 2), (3, 4, 5), (6, 7, 8),
+                              (0, 3, 6), (1, 4, 7), (2, 5, 8),
+                              (0, 4, 8), (2, 4, 6)]
+
+            for condition in win_conditions:
+                if self.board[condition[0]] == self.board[condition[1]] == self.board[condition[2]] != ' ':
+                    return self.board[condition[0]]
+            if ' ' not in self.board:
+                return 'Draw'
+            return False
+
+        def get_move(self):
+            while True:
+                move = input(f'Player {self.current_player}, enter your move (1-9): ')
+                if not move.isdigit() or int(move) < 1 or int(move) > 9 or self.board[int(move) - 1] != ' ':
+                    print('Invalid move. Try again.')
+                else:
+                    return int(move) - 1
