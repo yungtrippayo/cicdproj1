8e27038f836f77723071fe7db0af39353b332ff3
Никита Будовой <nikita.budovoy@knu.ua>
2023-05-31 19:30:04 +0300

    додали функцію  test_print_board

--- test_main.py
+++ test_main.py
@@ -9,5 +9,10 @@ def test_init():
     game = TicTacToe()
     assert game.board == [' '] * 9
     assert game.current_player == 'X'
+def test_print_board(capsys):
+    game = TicTacToe()
+    game.print_board()
+    captured = capsys.readouterr()
+    assert captured.out == '  |   |  \n---------\n  |   |  \n---------\n  |   |  \n'
 
 
